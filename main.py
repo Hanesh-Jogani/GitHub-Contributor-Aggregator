33d088634a9bcb7edf1a16af2edd3c77ff4fb2d1
Hanesh Jogani <hanesh.jogani@gmail.com>
2021-08-06 20:03:40 +0530

    Update main.py
    Added Date and Made Documentation

--- main.py
+++ main.py
@@ -8,11 +8,12 @@ for all the branches.
 ID, Repo Name, Domain Name, Count, Date range parameter, Date of Run collected using API.
 """
 
-# Importing Necessary Packages
+# Importing Necessary Packages/Libraries
 import requests
 import pandas as pd
+from datetime import date
 
-# Input - GitHub Username & Date
+# Input - GitHub Username & Date Range
 # Repo_Name = input("Enter the GitHub Repository Name (Username/Repo Name):- ")
 # Start_Date = input("Enter the Start Date (YYYY-MM-DD):- ")
 # End_Date = input("Enter the End Date (YYYY-MM-DD):- ")
@@ -20,47 +21,69 @@ import pandas as pd
 
 # Pre-Input Data
 Repo_Name = 'hashicorp/consul'  # Repository Name
-Start_Date = '2021-07-07'       # Start Date of Range
-End_Date = '2021-07-09'         # End Date of Range
+Start_Date = '2021-04-01'       # Start Date of Range
+End_Date = '2021-08-05'         # End Date of Range
 
-Data_Frame = pd.DataFrame()
-Output_Json = {}
+Auth = {'Authorization': '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'}    # Authorization Token
+Run_Date = date.today()         # Run_Date to get today's date
+Data_Frame = pd.DataFrame()     # Creating Pandas DataFrame to store in the data
+Output_Json = {}                # Output_Json will contain the main output of the code
+Length_Sum = 0                  # To keep the count of the Total Contributions made
+Unique_Sum = 0                  # To keep the count of the Unique Contributors
+page_number = 1
 
 # Fetching the JSON Data from the API
-# URL to which the Request will be sent to
-Auth = {'Authorization': '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'}
-url = f"https://api.github.com/repos/{Repo_Name}/commits?since={Start_Date}&until={End_Date}"
+while True:
+    # URL to which the Request will be sent to
+    url = f"https://api.github.com/repos/{Repo_Name}/commits?page={page_number}&since={Start_Date}&until={End_Date}"
 
-# Making a GET Request to the GitHub API
-request = requests.get(url, Auth).json()
-length = len(request)       # To examine the Total Commits taken place
+    # URL to which the Request will be sent to
+    request = requests.get(url, Auth).json()
+    length = len(request)
+    Length_Sum += length
 
-# Checking if the request was received or not
-if request:
-    # Concatenating the JSON Data into the Data Frame and re-setting its Index
-    Data_Frame = pd.concat([Data_Frame, pd.DataFrame(request)], ignore_index=True)
+    # Checking if the request was received or not
+    if request:
+        # Concatenating the JSON Data into the Data Frame and re-setting its Index
+        Data_Frame = pd.concat([Data_Frame, pd.DataFrame(request)], ignore_index=True)
 
-New_Data_Frame = Data_Frame
+        # Incrementing the Page Number
+        page_number = page_number + 1
+    else:
+        break
 
+New_Data_Frame = Data_Frame     # Copying it to New DataFrame
+
+# Processing the JSON Data Received from the API
 for request in New_Data_Frame['commit']:
     Email_Id = request['author']['email']       # Taking the Email ID of the Author
     Email_End = Email_Id.split('@')             # Splitting the Email ID into Username and Domain Name
-    Company = Email_End[1].rsplit('.', 1)       # Splitting the Domain Name into the Company Name and the extension
+
+    Company = Email_End[1].rsplit('.', 1)       # Splitting the Domain Name into the Company Name and the Extension
     Company_Name = Company[0]                   # Storing the Company Initial Name
+
+    Full_Date = request['author']['date']       # Taking the Date and Time when the commit took place
+    Only_Date = Full_Date.split('T')            # Splitting the Full_Date into Date and the Time
+    Date = Only_Date[0]                         # Storing the Date only
 
     if Company_Name in Output_Json.keys():
         Output_Json[Company_Name]['Total Contributions'] = Output_Json[Company_Name]['Total Contributions'] + 1
     else:
-        Output_Json[Company_Name] = {'Total Contributions': 1, 'Unique Contributors': 0, 'Users': []}
+        Output_Json[Company_Name] = {'Total Contributions': 1, 'Unique Contributors': 0, 'Users': [], 'Date': []}
 
     if Email_Id not in Output_Json[Company_Name]['Users']:
         Output_Json[Company_Name]['Users'].append(Email_Id)
         Output_Json[Company_Name]['Unique Contributors'] = Output_Json[Company_Name]['Unique Contributors'] + 1
+        Unique_Sum += 1
+
+    if Date not in Output_Json[Company_Name]['Date']:
+        Output_Json[Company_Name]['Date'].append(Date)
 
 # Output -  Company Domain Name, Total number of Commits & Total number of Contributors
-print(f'Total Commits:- {length}')
-print(f'Total Companies:- {list(Output_Json.keys())}')
-print(f'Output:- {Output_Json}')
+print(f'Total Companies:- {list(Output_Json.keys())}')  # Printing the Domain/Companies name
+print(f'Total Contributions:- {Length_Sum}')            # Printing the Count of Total Contributions made
+print(f'Unique Contributors:- {Unique_Sum}')            # Printing the Count of Unique Contributors who have contributed
+print(f'Output:- {Output_Json}')                        # Printing the Required Json Data
 
 
 """
